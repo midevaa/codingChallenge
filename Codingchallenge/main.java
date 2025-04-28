33291a0451b429c4ac5e97fd015da0a1b57849af
natalia malwa <natmalwa77@gmail.com>
2025-04-28 21:15:16 +0300

    Greeter class

--- Codingchallenge/main.java
+++
@@ -1,9 +0,0 @@
-package codingChallenge.Codingchallenge;
-
-public class main {
-    public static void main (String[]args){
-        HelloTillStop stop = new HelloTillStop();
-        stop.showHello();
-    }
-
-}
---
+++ greeter/Greeter.java
@@ -0,0 +1,12 @@
+package codingChallenge.greeter;
+
+public class Greeter {
+    void greet(){
+        System.out.println("Hi there");
+    }
+    public static void main(String[]args){
+        (new Greeter()).greet();
+    }
+}
+
+
