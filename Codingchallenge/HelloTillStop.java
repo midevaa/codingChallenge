d88363a37067b17c39d332e0798382ab50ad8943
natalia malwa <natmalwa77@gmail.com>
2025-04-28 20:19:50 +0300

    While loop hello till stop

---
+++ Codingchallenge/HelloTillStop.java
@@ -0,0 +1,19 @@
+package Codingchallenge;
+import java.util.*;
+
+public class HelloTillStop {
+    Scanner sc = new Scanner(System.in);
+
+    void showHello(){
+        String choice;
+
+        System.out.println("For Hello type h, anything else to stop");
+        choice = sc.next();
+        while ("h". equals(choice)){
+            System.out.println("Hello");
+            choice = sc.next();
+        }
+    }
+
+
+}
---
+++ Codingchallenge/main.java
@@ -0,0 +1,9 @@
+package Codingchallenge;
+
+public class main {
+    public static void main (String[]args){
+        HelloTillStop stop = new HelloTillStop();
+        stop.showHello();
+    }
+
+}
