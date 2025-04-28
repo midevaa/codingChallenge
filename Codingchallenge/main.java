35cb58f61aeb3531b0cd2d1f78e3fb907a88302b
natalia malwa <natmalwa77@gmail.com>
2025-04-28 21:18:41 +0300

    while loop hello till stop

---
+++ Codingchallenge/main.java
@@ -0,0 +1,7 @@
+package codingChallenge.Codingchallenge;
+
+public class main {
+    public static void main (String[]args){
+        HelloTillStop stop = new HelloTillStop();
+        stop.showHello();
+    }}
